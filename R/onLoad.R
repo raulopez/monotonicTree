aaa01e5fe1789ff8ecd35c2812c16a04a5682b1b
raul lopez <raulopez8930@gmail.com>
2016-08-22 19:34:26 +0200

    arreglos onLoad.R

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,5 +1,6 @@
 .onLoad <- function(libname = find.package("monotonicTree"), pkgname = "monotonicTree"){
   
+  cat("Download JAR..\n")
   dir.create(file.path(system.file(package = "monotonicTree"), "download"))
   path <- file.path(system.file(package = "monotonicTree"), "download","JarFiles.zip")
   downloader::download(url="https://github.com/raulopez/monotonicTree/raw/master/jar/JarFiles.zip",destfile = path, mode ="wb")
